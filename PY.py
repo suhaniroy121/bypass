678ffbfe4387f7a393869c038b2d83dc75f8edcc
suhaniroy121 <115403193+suhaniroy121@users.noreply.github.com>
2022-11-16 17:22:51 +0530

    Add files via upload

---
+++ PY.py
@@ -0,0 +1,54 @@
+import os
+os.system('pip install requests')
+os.system('clear')
+
+print(f"""
+\x1b[1;97m-------┏────────────────────────────────────────────────┓
+\x1b[1;90m-------│           \x1b[1;93m\x1b[1;104mᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ʀᴀɴɪ ᴡᴏʀʟᴅ\033[0;m\x1b[1;93m                │
+\x1b[1;95m-------┠─────────┯─────────┯─────────┯────────┯─────────┫
+\x1b[1;96m-------\x1b[1;95m│\x1b[1;93m##     ##\x1b[1;95m│  \x1b[1;96m####   \x1b[1;95m│ \x1b[1;95m######  \x1b[1;95m│\x1b[1;92m########\x1b[1;95m│\x1b[1;91m##    ## \x1b[1;95m│
+\x1b[1;94m-------\x1b[1;95m│\x1b[1;93m###   ###\x1b[1;95m│   \x1b[1;96m##    \x1b[1;95m│\x1b[1;95m##    ## \x1b[1;95m│  \x1b[1;92m ##  \x1b[1;95m │ \x1b[1;91m##  ##  \x1b[1;95m│
+\x1b[1;91m-------\x1b[1;95m│\x1b[1;93m#### ####\x1b[1;95m│   \x1b[1;96m##    \x1b[1;95m│\x1b[1;95m##       \x1b[1;95m│   \x1b[1;92m##   \x1b[1;95m│  \x1b[1;91m####   \x1b[1;95m│
+\x1b[1;92m-------\x1b[1;95m│\x1b[1;93m## ### ##\x1b[1;95m│   \x1b[1;96m##    \x1b[1;95m│ \x1b[1;95m###### \x1b[1;95m │   \x1b[1;92m##   \x1b[1;95m│   \x1b[1;91m##    \x1b[1;95m│
+\x1b[1;93m-------\x1b[1;95m│\x1b[1;93m##     ##\x1b[1;95m│   \x1b[1;96m##    \x1b[1;95m│      \x1b[1;95m## \x1b[1;95m│   \x1b[1;92m##   \x1b[1;95m│   \x1b[1;91m##    \x1b[1;95m│
+\x1b[1;95m-------\x1b[1;95m│\x1b[1;93m##     ##\x1b[1;95m│   \x1b[1;96m##    \x1b[1;95m│\x1b[1;95m##     ##\x1b[1;95m│   \x1b[1;92m##   \x1b[1;95m│   \x1b[1;91m##    \x1b[1;95m│
+\x1b[1;90m-------\x1b[1;95m│\x1b[1;93m##     ##\x1b[1;95m│  \x1b[1;96m####   \x1b[1;95m│ \x1b[1;95m######  \x1b[1;95m│   \x1b[1;92m##   \x1b[1;95m│   \x1b[1;91m##    \x1b[1;95m│
+\x1b[1;95m-------┠─────────┷─────────┷─────────┷─┬──────┷─────────┷──────────┓
+\x1b[1;97m-------│\x1b[1;95m❲◍❳ \x1b[1;91mᴅᴇᴠᴏʟᴏᴘᴇʀ: \x1b[1;92m\x1b[1;45m ʀᴀɴɪ ᴋᴜᴍᴀʀɪ\033[0;m\x1b[1;92m    │\x1b[1;91m● \x1b[1;96mɴᴏᴛᴇ:\x1b[1;92mᴛʜɪs \x1b[1;94mᴛᴏᴏʟs \x1b[1;91mɪs \x1b[1;93mғʀᴇᴇ \x1b[1;91m●│
+\x1b[1;91m-------┠───────────────────────────────╂───────────────────────────┫
+\x1b[1;92m-------│\x1b[1;95m❲◍❳ \x1b[1;91mɢɪᴛʜᴜʙ   : \x1b[1;93mʀᴀɴɪ\x1b[1;96msʜᴀʀᴍᴀ9     │\x1b[1;91m● \x1b[1;96mᴛʜᴇ \x1b[1;90mǫᴜᴇᴇɴ \x1b[1;91mᴏғғ \x1b[1;93mᴘʀᴏɢʀᴀᴍᴍᴇʀ\x1b[1;91m●│
+\x1b[1;93m-------┠───────────────────────────────╂─────────────┯─────────────┫
+\x1b[1;94m-------│\x1b[1;95m❲◍❳ \x1b[1;91mғᴀᴄᴇʙᴏᴏᴋ : \x1b[1;90mʀᴀɴɪ \x1b[1;91mxᴅ \x1b[1;90mᴍɪsᴛʏ   \x1b[1;95m│\x1b[1;93m● \x1b[1;96m● \x1b[1;93m● ┏──────┷──────┓ \x1b[1;93m● \x1b[1;96m● \x1b[1;93m●│
+\x1b[1;95m-------┠───────────────────────────────┫\x1b[1;97m● \x1b[1;91m● \x1b[1;97m●  \x1b[1;90mʀᴀɴɪ \x1b[1;91mx \x1b[1;90mᴍɪsᴛʏ   \x1b[1;97m● \x1b[1;91m● \x1b[1;97m●│
+\x1b[1;90m-------│\x1b[1;95m❲◍❳ \x1b[1;91mᴡʜᴀᴛsᴀᴘᴘ : \x1b[1;92m+ɴᴏ             │\x1b[1;92m● \x1b[1;94m● \x1b[1;92m● ┗─────────────┛ \x1b[1;92m● \x1b[1;94m● \x1b[1;92m●│
+\x1b[1;96m-------┠───────────────────────────────┷───────┯───────────────────┫
+\x1b[1;97m-------│\x1b[1;95m❲◍❳ \x1b[1;91mᴛᴇʟᴇɢʀᴀᴍ : \x1b[1;92mɴᴏ                      │\x1b[1;91m● \x1b[1;96mᴛʜᴀɴᴋs \x1b[1;94mᴇᴠᴇʀʏᴏɴᴇ \x1b[1;91m●│
+\x1b[1;91m-------┗───────────────────────────────────────┷───────────────────┛""")
+os.system("play-audio WLCM_TO_BYPASS_WORLD.mp3")
+print ('\x1b[1;93m NOTE :-FIRST MAHADI PROTECT ON SEC BYPASS CHOSE')
+##print('\x1b[1;91m\n[1] RIAZ \x1b[1;92mv3.8.1\n\x1b[1;91m[2] FB-KING \x1b[1;92mv2.0.8 \n\x1b[1;91m[3] HOP \x1b[1;92mv6.3 \n\x1b[1;91m[4] BXB \x1b[1;92mv0.1.7 \n\x1b[1;91m[5] MAHADI protect 64 bit \n\x1b[1;91m[6] MAHADI protect 32bit\n\x1b[1;91m[0] Exit')
+print ('\x1b[1;91m\n[1] RIAZ \x1b[1;92mv3.8.1')
+print ('\x1b[1;91m[2] FB-KING \x1b[1;92mv2.0.8 ')
+print ('\x1b[1;91m[3] HOP \x1b[1;92mv6.3')
+print ('\x1b[1;91m[4] BXB \x1b[1;92mv0.1.7')
+print ("\x1b[1;91m[5] SPY \x1b[1;92mv NEW BYPASS ")
+print ("\x1b[1;91m[6] RIAZ \x1b[1;92mV3.9.1 NEW BYPASS")
+print ('\x1b[1;91m[7] NOX \x1b[1;92m bit 64NEW BYPASS ')
+print ('\x1b[1;91m[8] NOX \x1b[1;92m bit 32NEW BYPASS ')
+print ('\x1b[1;91m[9] MAHADI protect \x1b[1;92m on 64 bit')
+print ('\x1b[1;91m[10] MAHADI protect \x1b[1;92m  on 32bit')
+print ('\x1b[1;91m[0] Exit')
+
+x = input('\n[•] Option: ')
+
+if x in ('1', '01'):
+    xx = 'FUCK YOU BYPASS WORLD'
+    #os.system("play-audio riaz.mp3")
+    if not os.path.isfile('zain.so'):
+              os.system('curl -L https://github.com/ranisharma09/byp4ss/blob/main/zain.cpython-310.so?raw=true -o MISTYY2.so') 
+       # import MISTYY2
+              import zain
+
+	#exit()
+  ## return None
+None('[•] See U Again  ')
